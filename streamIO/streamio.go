739a18076731835ef9517051ec741701bf12ab21
BYT3F0X <matheus.x@mobilitee.com.br>
2019-07-22 01:03:19 -0300

    first works

---
+++ streamIO/streamio.go
@@ -0,0 +1,35 @@
+// streamio this package contains utilities for dealing with binary streams in go
+package streamio
+
+import (
+	"errors"
+	"io"
+)
+
+// readBytes the primitive function used by this library to read streams in chunks which may be offset.
+// returns bytes read, number of bytes read and any errors that may have occurred.
+func readBytes(reader io.Reader, length int, offset int) ([]byte, int, error) {
+	bytes := make([]byte, length)
+
+	if offset != 0 {
+		switch r := reader.(type) {
+		case io.Seeker:
+			_, err := r.Seek(0, offset)
+			if err != nil {
+				return bytes, -1, err
+			}
+		default:
+			return bytes, -1, errors.New("the stream is not seekable")
+		}
+	}
+
+	// From this point on if a error is returned the bytes array may not be empty
+	retNum, err := reader.Read(bytes)
+
+	if err != nil {
+		return bytes, retNum, err
+	}
+
+	return bytes, retNum, nil
+}
+
